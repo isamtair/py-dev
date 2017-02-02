c89e6ea3d34028780d3e527fac1dbc555173b6ed
isamtair <isamtair@yahoo.com>
2017-02-02 16:26:45 +0200

    Commiting from pyCharm first try

--- 1.py
+++ 1.py
@@ -1,2 +1,22 @@
+#First python
+import sys
+from os import path
+
 print ("Hi")
-print ("Isam")
+print "Lets see what names you have in mind"
+
+
+
+def read_names():
+    names = []
+    leave = 'exit'
+    while True:
+        name = raw_input("Enter your name: ")
+        if name==leave:
+            print "Leaving now, GoodBye !!!"
+            exit()
+        else:
+            names.append(name)
+            print names[0], len(name), len(names)
+read_names()
+
